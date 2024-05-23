97151f76a1a0641d39b1ba0dc3961dc3e4b193bd
Undersigned <108613065+leapfrog7@users.noreply.github.com>
2024-05-23 19:47:04 +0530

    Update c.js

--- JS/c.js
+++ JS/c.js
@@ -228,6 +228,7 @@ var Global_C = [
     ['rkm131','rkm44956',1],
     ['nishkam179','nishkam75750',1],
     ['preety113','preety15156',1],
+    ['naman105','naman15752',1],
     
     ['amit130','amit34951',1],
     ['nidhi133','nidhi35754',1],
